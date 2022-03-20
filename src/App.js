1c27f46bee0b7acfbac285ea165cc72a8832dbed
AtoBrightSide <meseleabel99@gmail.com>
2022-03-20 16:35:09 +0300

    changed card layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FileUploadScreen from './screens/uploadScreen';
-import {getMultipleFiles} from './data/api';
+import { getMultipleFiles } from './data/api';
+import { Car, CarContainer, CarImage, RemoveCarButton } from './styles/CarCard.styled';
 
-function App() {;
+function App() {
   const [multipleFiles, setMultipleFiles] = useState([]);
 
   const getMultipleFilesList = async () => {
     try {
-        const fileslist = await getMultipleFiles();
-        setMultipleFiles(fileslist);
-        console.log(multipleFiles);
+      const fileslist = await getMultipleFiles();
+      setMultipleFiles(fileslist);
+      console.log(multipleFiles);
     } catch (error) {
       console.log(error);
     }
@@ -20,32 +21,22 @@ function App() {;
   }, []);
   return (
     <>
-        <div className="container">
-          <h3 className="text-danger font-weight-bolder border-bottom text-center">Car Data Upload </h3>
-          <FileUploadScreen getMultiple={() => getMultipleFilesList()}/>
-       </div> 
-       <div className="container-fluid mt-5">
-         <div className="row">
-          
-           <div className="col-6">
-             <h4 className="text-success font-weight-bold">Cars List</h4>
-             {multipleFiles.map((element, index) =>
-                <div key={element._id}>
-                    <h6 className="text-danger font-weight-bold">{element.carId}</h6>
-                    <div className="row">
-                      {element.files.map((file, index) =>
-                        <div className="col-6">
-                            <div className="card mb-2 border-0 p-0">
-                              <img src={`http://localhost:5000/${file.filePath}`} height="200" className="card-img-top img-responsive" alt="img"/>
-                              </div>
-                          </div>
-                       )}
-                      </div>
-                </div>
-              )}
-           </div>
-         </div>
-       </div>
+      <div className="container">
+        <h3 className="text-danger font-weight-bolder border-bottom text-center">Upload Car</h3>
+        <FileUploadScreen getMultiple={() => getMultipleFilesList()} />
+      </div>
+      <br />
+      <h3 className="text-danger font-weight-bold text-center">Cars on DB</h3>
+      <br />
+      <CarContainer>
+        {multipleFiles.map((element, index) =>
+          <Car key={element._id}>
+            <h3 className="text-danger font-weight-bold">{element.make}</h3>
+            <CarImage src={`http://localhost:5000/${element.files[0].filePath}`} height="200" alt="img" />
+            <RemoveCarButton>Remove Car</RemoveCarButton>
+          </Car>
+        )}
+      </CarContainer>
     </>
   );
 }
---
+++ src/styles/CarCard.styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const CarContainer = styled.div`
+  display: grid;
+  gap: 10px 10px;
+  grid-template-columns: auto auto auto;
+  justify-content: space-evenly;
+`;
+
+export const Car = styled.div`
+  height: 300px;
+  width: 300px;
+  background-color: rgba(50,50,150,0.4);
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const CarImage = styled.img`
+    align-self: center;
+    justify-self: center;
+`;
+
+export const RemoveCarButton = styled.button`
+    background-color: #ff0000;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    /* align-self: flex-end; */
+    justify-self: center;
+`;
