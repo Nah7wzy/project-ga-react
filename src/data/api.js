5913e4ee0a81658d64ef3d78fab36c240add17dc
Nahom Tamru <61546296+Nah7wzy@users.noreply.github.com>
2023-01-08 15:26:13 +0300

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://getzcarsellbackend.onrender.com/';
+const apiUrl = 'http://getzcarsellbackend.onrender.com/cars/';
 
 
 export const multipleFilesUpload = async (data, options) => {
